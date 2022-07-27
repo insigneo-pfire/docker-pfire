47f053b848a2d38dbc39a752cb6829e26d391e58
Daniele Tartarini <d.tartarini@sheffield.ac.uk>
2022-07-27 17:22:11 +0100

    some more work

--- singularity/build_pfire_singularity.bash
+++ singularity/build_pfire_singularity.bash
@@ -3,5 +3,5 @@
 singularity build -F base_centos8_for_pfire.sif  base_centos8_for_pfire.def  
 singularity build -F base_centos8_openmpi_for_pfire.sif  base_centos8_openmpi_for_pfire.def 
 singularity build -F base_openmpi_petsc_for_pfire.sif base_openmpi_petsc_for_pfire.def  
-singularity build -F base_petsc_for_pfire.sif  base_petsc_for_pfire.def  
+#singularity build -F base_petsc_for_pfire.sif  base_petsc_for_pfire.def  
 singularity build -F pfire_openmpi.sif pfire_openmpi.def
