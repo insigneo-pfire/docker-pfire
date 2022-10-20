2c5d60ac07d3bd60cfd85516c722c5476bd4ea9e
Daniele Tartarini <d.tartarini@sheffield.ac.uk>
2022-10-20 11:58:25 +0100

    restructuring and cleanup

--- singularity/build_pfire_singularity.bash
+++ singularity/build_pfire_singularity.bash
@@ -3,5 +3,7 @@
 singularity build -F base_centos8_for_pfire.sif  base_centos8_for_pfire.def  
 singularity build -F base_centos8_openmpi_for_pfire.sif  base_centos8_openmpi_for_pfire.def 
 singularity build -F base_openmpi_petsc_for_pfire.sif base_openmpi_petsc_for_pfire.def  
-#singularity build -F base_petsc_for_pfire.sif  base_petsc_for_pfire.def  
+#singularity build -F base_petsc_for_pfire.sif  base_petsc_for_pfire.def
+
+singularity build -F base_openmpi_petsc_dcmtk_oiio_for_pfire.sif base_openmpi_petsc_dcmtk_oiio_for_pfire.def
 singularity build -F pfire_openmpi.sif pfire_openmpi.def
